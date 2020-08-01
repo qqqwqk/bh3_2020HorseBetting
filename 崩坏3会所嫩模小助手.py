305b341f529020cf2f6b542358ef256b4f543d93
qqqwqk <18205617550@163.com>
2020-08-01 23:09:49 +0800

    Update 崩坏3会所嫩模小助手.py

--- 崩坏3会所嫩模小助手.py
+++ 崩坏3会所嫩模小助手.py
@@ -22,49 +22,62 @@ def player1skill0(ch):
         pass
     elif (ch==1):#30%概率令对方跳过下回合
         if(random.randint(0,100)<30):
+            print(player1.name+'成功发动'+player1.skill0name,end='  ')
             player2.jumpround=1        
     elif (ch==2):#25%概率连续4次12伤害的攻击（会计算护甲）
         if(random.randint(0,100)<25):
+            print(player1.name+'成功发动'+player1.skill0name,end='  ')
             for i in range(0,4):#连续4次
                 if(random.randint(0,100)<player1.hit*100):#命中计算
                     player2.hp-=12-player2.defence      
     elif (ch==3):#对手为5，7，8，10时 攻击翻倍
         if(iplayer2==5 or iplayer2==7 or iplayer2==8 or iplayer2==10):
+            print(player1.name+'成功发动'+player1.skill0name,end='  ')
             player1.attack*=2
     elif (ch==4):#35%概率 该次伤害降低3点 但对手永久降低攻击4点
         if(random.randint(0,100)<35):
+            print(player1.name+'成功发动'+player1.skill0name,end='  ')
             if(random.randint(0,100)<player1.hit*100):
                 player2.hp-=player1.attack-3
                 player2.attack-=4
     elif (ch==5):#30%概率回复25生命
         if(random.randint(0,100)<30):
+            print(player1.name+'成功发动'+player1.skill0name,end='  ')
             player1.hp+=25
     elif (ch==6):#若对手是0 输出提升25% 否则有25%概率输出提升25%
         if(iplayer2==0):
+            print(player1.name+'成功发动'+player1.skill0name,end='  ')
             player1.attack*=1.25
         else:
             if(random.randint(0,100)<25):
+                print(player1.name+'成功发动'+player1.skill0name,end='  ')
                 player1.attack*=1.25
     elif (ch==7):#进行一次攻击判定后 对手永久降防5点
-        if(random.randint(0,100)<player1.hit*100):
-            player2.hp-=player1.attack-player2.defence
+        #if(random.randint(0,100)<player1.hit*100):
+            #player2.hp-=player1.attack-player2.defence
         if(random.randint(0,100)<30):
+            print(player1.name+'成功发动'+player1.skill0name,end='  ')
             player2.defence-=5
     elif (ch==8):#自身生命恢复至20点
         if(player1.cold==9999):
+            print(player1.name+'成功发动'+player1.skill0name,end='  ')
             player1.hp=20
             player1.cold=1
     elif (ch==9):#单数回合时伤害提升10但防御下降5 双数回合时随机恢复0-15生命 伤害降低10 防御提升5
         if(round%2==1):
+            print(player1.name+'成功发动'+player1.skill0name,end='  ')
             player1.attack+=10
             player1.defence-=5
         else:
+            print(player1.name+'成功发动'+player1.skill1name,end='  ')
             player1.hp+=random.randint(0,15)
             player1.attack-=10
             player1.defence+=5
     elif (ch==10):#自身伤害提升3点
+        print(player1.name+'成功发动'+player1.skill0name,end='  ')
         player1.attack+=3
     elif (ch==11):#自身获得元素穿透
+        print(player1.name+'成功发动'+player1.skill0name,end='  ')
         player1.elemental=1
 
 def player2skill0(ch):
@@ -72,49 +85,62 @@ def player2skill0(ch):
         pass
     elif (ch==1):
         if(random.randint(0,100)<30):
+            print(player2.name+'成功发动'+player2.skill0name,end='  ')
             player1.jumpround=1
     elif (ch==2):
         if(random.randint(0,100)<25):
+            print(player2.name+'成功发动'+player2.skill0name,end='  ')
             for i in range(0,4):#连续4次
                 if(random.randint(0,100)<player2.hit*100):#命中计算
                     player1.hp-=12-player1.defence
     elif (ch==3):
         if(iplayer1==5 or iplayer1==7 or iplayer1==8 or iplayer1==10):
+            print(player2.name+'成功发动'+player2.skill0name,end='  ')
             player2.attack*=2
     elif (ch==4):
         if(random.randint(0,100)<35):
+            print(player2.name+'成功发动'+player2.skill0name,end='  ')
             if(random.randint(0,100)<player2.hit*100):
                 player1.hp-=player2.attack-3
                 player1.attack-=4
     elif (ch==5):
         if(random.randint(0,100)<30):
+            print(player2.name+'成功发动'+player2.skill0name,end='  ')
             player2.hp+=25
     elif (ch==6):
         if(iplayer1==0):
+            print(player2.name+'成功发动'+player2.skill0name,end='  ')
             player2.attack*=1.25
         else:
             if(random.randint(0,100)<25):
+                print(player2.name+'成功发动'+player2.skill0name,end='  ')
                 player2.attack*=1.25
     elif (ch==7):
-        if(random.randint(0,100)<player2.hit*100):
-            player1.hp-=player2.attack-player1.defence
+        #if(random.randint(0,100)<player2.hit*100):
+            #player1.hp-=player2.attack-player1.defence
         if(random.randint(0,100)<30):
+            print(player2.name+'成功发动'+player2.skill0name,end='  ')
             player1.defence-=5
     elif (ch==8):
         if(player2.cold==9999):
+            print(player2.name+'成功发动'+player2.skill0name,end='  ')
             player2.hp=20
             player2.cold=1
     elif (ch==9):
         if(round%2==1):
+            print(player2.name+'成功发动'+player2.skill0name,end='  ')
             player2.attack+=10
             player2.defence-=5
         else:
+            print(player2.name+'成功发动'+player2.skill1name,end='  ')
             player2.hp+=random.randint(0,15)
             player2.attack-=10
             player2.defence+=5
     elif (ch==10):
+        print(player2.name+'成功发动'+player2.skill0name,end='  ')
         player2.attack+=3
     elif (ch==11):
+        print(player2.name+'成功发动'+player2.skill0name,end='  ')
         player2.elemental=1
 def player1skill1(ch):
     if(ch==0):
@@ -134,6 +160,15 @@ def player1skill1(ch):
     elif (ch==3):
         player1.attack*=2
         player1.hit*=0.65
+        print(player1.name+"使用普攻")
+        if(random.randint(0,100)<player1.hit*100):
+            player2.hp-=player1.attack-(1-player1.elemental)*player2.defence
+                        #丽塔         #芽衣
+            if(iplayer1==4 or iplayer1==1):
+                player1skill0(iplayer1)
+            print(player2.name+"剩余血量"+str(player2.hp))
+        else:
+            print("但是被闪避了")
     elif (ch==4):
         player2.hp+=4
         player2.banskill=1
@@ -178,6 +213,15 @@ def player2skill1(ch):
     elif (ch==3):
         player2.attack*=2
         player2.hit*=0.65
+        print(player2.name+"使用普攻")
+        if(random.randint(0,100)<player2.hit*100):
+            player1.hp-=player2.attack-(1-player2.elemental)*player1.defence
+                        #丽塔         #芽衣
+            if(iplayer2==4 or iplayer2==1):
+                player2skill0(iplayer2)
+            print(player1.name+"剩余血量"+str(player1.hp))
+        else:
+            print("但是被闪避了")
     elif (ch==4):
         player1.hp+=4
         player1.banskill=1
@@ -209,8 +253,8 @@ def player2skill1(ch):
 
 def player1_attack_player2():
     #攻击前
-                #呆鹅          #希儿          #渡鸦         #樱莲
-    if(iplayer1==10 or iplayer1==9 or iplayer1==6 or iplayer1==5):
+                #呆鹅          #希儿         #樱莲
+    if(iplayer1==10 or iplayer1==9 or iplayer1==5):
         player1skill0(iplayer1)
 
     #攻击中
@@ -222,15 +266,15 @@ def player1_attack_player2():
                     print(player2.name+"发动技能"+player2.skill1name)
                     player2skill1(iplayer2)
                 else:
-                    print(player1.name+"发动技能"+player1.skill1name)
+                    print(player1.name+"发动技能"+player1.skill1name,end='  ')
                     player1skill1(iplayer1)
                     print(player2.name+"剩余血量"+str(player2.hp))
             else:
-                print(player1.name+"使用普攻")
+                print(player1.name+"使用普攻",end='  ')
                 if(random.randint(0,100)<player1.hit*100):
                     player2.hp-=player1.attack-(1-player1.elemental)*player2.defence
-                                #丽塔         #芽衣
-                    if(iplayer1==4 or iplayer1==1):
+                               #德莉莎         #丽塔         #芽衣
+                    if(iplayer1==7 or iplayer1==4 or iplayer1==1):
                         player1skill0(iplayer1)
                     print(player2.name+"剩余血量"+str(player2.hp))
                 else:
@@ -242,11 +286,11 @@ def player1_attack_player2():
 
     else:
         player1.jumpround=0
-        print(player1.name+"恢复行动了")
+        print(player1.name+"跳过了回合")
 
     #攻击后
-            #德莉莎          #板鸭
-    if(iplayer1==7 or iplayer1==2 ):
+                     #板鸭
+    if(iplayer1==2 ):
         player1skill0(iplayer1)   
     #承伤复活判定
     if(iplayer2==8 and player2.hp<=0):
@@ -254,8 +298,8 @@ def player1_attack_player2():
 
 def player2_attack_player1():
     #攻击前
-                #呆鹅          #希儿          #渡鸦         #樱莲
-    if(iplayer2==10 or iplayer2==9 or iplayer2==6 or iplayer2==5):
+                #呆鹅          #希儿           #樱莲
+    if(iplayer2==10 or iplayer2==9 or iplayer2==5):
         player2skill0(iplayer2)
 
     #攻击中
@@ -268,15 +312,15 @@ def player2_attack_player1():
                         print(player1.name+"发动技能"+player1.skill1name)
                         player1skill1(iplayer1)
                     else:
-                        print(player2.name+"发动技能"+player2.skill1name)
+                        print(player2.name+"发动技能"+player2.skill1name,end='  ')
                         player2skill1(iplayer2)
                         print(player1.name+"剩余血量"+str(player1.hp))
                 else:
-                    print(player2.name+"使用普攻")
+                    print(player2.name+"使用普攻",end='  ')
                     if(random.randint(0,100)<player2.hit*100):
                         player1.hp-=player2.attack-(1-player2.elemental)*player1.defence
-                                    #丽塔         #芽衣
-                        if(iplayer2==4 or iplayer2==1):
+                                  #德莉莎       #丽塔         #芽衣
+                        if(iplayer2==7 or iplayer2==4 or iplayer2==1):
                             player2skill0(iplayer2)
                         print(player1.name+"剩余血量"+str(player1.hp))
                     else:
@@ -291,8 +335,8 @@ def player2_attack_player1():
         print(player2.name+"恢复行动了")
 
     #攻击后
-            #德莉莎          #板鸭
-    if(iplayer2==7 or iplayer2==2 ):
+                 #板鸭
+    if(iplayer2==2 ):
         player2skill0(iplayer2)   
     #承伤复活判定
     if(iplayer1==8 and player1.hp<=0):
@@ -321,7 +365,7 @@ times=int(input())
 player1win=0
 player2win=0
 for i in range(1,times+1):
-    
+    print('\r\n第'+str(i)+'次模拟')
     class kiana:
         name='琪亚娜'
         banskill=0
@@ -530,25 +574,31 @@ for i in range(1,times+1):
     print(player1.name+" 对阵 "+player2.name+" !" )
 #    player1.hp=100
 #    player2.hp=100
-    round=1
+
+    print("round0")
+    if(iplayer1==3 or iplayer1==11 or iplayer1==6 ):
+        player1skill0(iplayer1)
+    if(iplayer2==3 or iplayer2==11 or iplayer2==6 ):
+        player2skill0(iplayer2)
+    print()  
+    round=1  
     while(player1.hp>0 and player2.hp>0):
         #开战前
-        if(iplayer1==3 or iplayer1==11):
-            player1skill0(iplayer1)
-        if(iplayer2==3 or iplayer2==11):
-            player2skill0(iplayer2)
+        
 
         if(player1.speed>player2.speed):
-            
+            print("round"+str(round))
+            #print("round"+str(round)+".1")
+
             player1_attack_player2()
 
             if(player2.hp>0):
-                print("round"+str(round)+".1"+player2.name+"剩余血量"+str(player2.hp))
                 
+                #print("round"+str(round)+".2")
+
                 player2_attack_player1()
                 
                 if(player1.hp>0):
-                    print("round"+str(round)+".2"+player1.name+"剩余血量"+str(player1.hp))
                     round+=1
                 else:
                     print("game is over,winner is"+player2.name)
@@ -560,12 +610,19 @@ for i in range(1,times+1):
                 player1win+=1
                 break
         else:
+            print("round"+str(round))
+            #print("round"+str(round)+".1")
+
             player2_attack_player1()
+
             if(player1.hp>0):
-                print("round"+str(round)+".1"+player1.name+"剩余血量"+str(player1.hp))
+                
+                #print("round"+str(round)+".2")
+
                 player1_attack_player2()
+
                 if(player2.hp>0):
-                    print("round"+str(round)+".2"+player2.name+"剩余血量"+str(player2.hp))
+                    
                     round+=1
                 else:
                     print("game is over,winner is"+player1.name)
